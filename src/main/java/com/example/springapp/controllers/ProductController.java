f553c9f721b0f084e422a2fd00473884438f74f8
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-10 00:06:53 -0300

    Initial commit

---
+++ src/main/java/com/example/springapp/controllers/ProductController.java
@@ -0,0 +1,28 @@
+package com.example.springapp.controllers;
+
+import com.example.springapp.dtos.ProductRecordDto;
+import com.example.springapp.models.ProductModel;
+import com.example.springapp.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+}
---
+++ src/main/java/com/example/springapp/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.example.springapp.repositories;
+
+import com.example.springapp.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+
+}
