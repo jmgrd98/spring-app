c21675d7cc993c13749b7ebd09fad2d0bd3a60b0
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-10 00:24:45 -0300

    Update ProductController.java

--- src/main/java/com/example/springapp/controllers/ProductController.java
+++ src/main/java/com/example/springapp/controllers/ProductController.java
@@ -4,14 +4,16 @@ import com.example.springapp.dtos.ProductRecordDto;
 import com.example.springapp.models.ProductModel;
 import com.example.springapp.repositories.ProductRepository;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductController {
@@ -26,8 +28,17 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
 
-    @GetMapping('/products')
-    public ResponseEntity<ProductModel> getProducts() {
-        return ResponseEntity.status(HttpStatus.FOUND).
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Object> getProduct(@PathVariable(value="id") UUID id) {
+        Optional<ProductModel> product0 = productRepository.findById(id);
+        if(product0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(product0.get());
     }
 }
